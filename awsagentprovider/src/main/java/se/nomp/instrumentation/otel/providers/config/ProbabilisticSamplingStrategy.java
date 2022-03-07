e2036bd7537b3dfcd03ac17385055d46ee9ae817
stnor <stefan@selessia.com>
2022-03-07 19:26:04 +0100

    Städat lite och rättat path på ConfigurableSampleProvider i META-INF

--- awsagentprovider/src/main/java/se/nomp/instrumentation/otel/providers/config/ProbabilisticSamplingStrategy.java
+++
@@ -1,9 +0,0 @@
-package se.nomp.instrumentation.otel.providers.config;
-
-public class ProbabilisticSamplingStrategy {
-    double samplingRate;
-
-    public double getSamplingRate() {
-        return samplingRate;
-    }
-}
